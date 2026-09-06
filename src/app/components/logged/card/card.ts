fe7b5569707cf06396fa70adcf509c309e6fa843
Samuel <samuel@gmail.com>
2026-09-06 15:40:23 -0300

    feat: Cria os cards na tela logada exibindo todos os filmes que não puchados do home da parte logada, configura todo css, html e ts da pagina

--- src/app/components/logged/card/card.ts
+++ src/app/components/logged/card/card.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  imports: [],
+  standalone: true,
+  imports: [NgIf],
   selector: 'app-card',
   styleUrl: './card.css',
   templateUrl: './card.html',
 })
-export class Card {}
+export class Card {
+  @Input() title!: string;
+  @Input() description!: string;
+  @Input() duration!: string;
+  @Input() ageRating!: string;
+  @Input() approvalRating!: number;
+  @Input() providerLogo!: string;
+  @Input() isTop10 = false;
+}
